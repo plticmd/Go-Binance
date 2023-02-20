3814a96a0fe888726b0e0bd03b76b9fc20151170
plticmd <86671100+plticmd@users.noreply.github.com>
2023-02-20 17:18:33 +0900

    Add files via upload

---
+++ contracts/erc20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+// pragma solidity ^0.8.0;
+
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.8/contracts/token/ERC20/ERC20.sol";
+
+// contract Token is ERC20 {
+
+//     constructor () ERC20("Tokem", "TKN") {
+//         _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+//     }
+// }
